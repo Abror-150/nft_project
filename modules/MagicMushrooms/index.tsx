db96d7910f17048586c640bcaf1e5a50ab29247e
Abror-150 <urazalievv.abror@gmail.com>
2025-07-18 14:18:19 +0500

    added

---
+++ modules/MagicMushrooms/index.tsx
@@ -0,0 +1,10 @@
+import { getRequestServer } from "@/service/getRequest";
+import MushroomsContent from "./MushroomContent";
+
+
+const MagicMushrooms = async () => {
+    const mushroomData = await getRequestServer("/auctions?limit=1")
+    return <MushroomsContent mushroomData={mushroomData}/>
+};
+
+export default MagicMushrooms;
